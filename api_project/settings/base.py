cc2b310400df883d9292df76883e647211ef7849
KarolTrevizani <karolineh.trevizani@gmail.com>
2023-07-19 17:44:19 -0300

    DockerFile config

--- api_project/settings/base.py
+++ api_project/settings/base.py
@@ -86,14 +86,14 @@ WSGI_APPLICATION = "api_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": "mydatabase",
-#     }      
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "mydatabase",
+    }      
+}
 
-DATABASES = {"default": env.db("DATABASE_URL")}
+# DATABASES = {"default": env.db("DATABASE_URL")}
 
 PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.PBKDF2PasswordHasher",
